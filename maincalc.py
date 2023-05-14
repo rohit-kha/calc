b50cf11c5dd31887d902a3bdc9a6fb26b5c3d30b
rohit <khadkarohit235@gmail.com>
2023-05-14 17:10:07 +0545

    Main file done

--- maincalc.py
+++ maincalc.py
@@ -1,1 +1,11 @@
-# This is written by mr. Rohit Khadka
+import addition
+import division
+import multiply
+import subtraction
+
+a = 100
+b = 50
+
+total_sum = add_work.add(a,b)
+total_sum = sub_work.sub(a,b)
+total_mul = mul_work.mul(a,b)
